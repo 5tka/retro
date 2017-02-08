4db270c06bb107cc0af9f7087e91ec6c2f5fab65
5tka <ferol2010@yandex.ru>
2017-02-08 23:43:12 +0200

    header

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
 console.log('hello from script.js');
+
+
+
+$( document ).ready(function() { // начало 
+
+    $('.dd-tr').click(function(){
+        $(this).toggleClass('tr180');
+        $('.dd').stop(true).slideToggle('slow');
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // конец
